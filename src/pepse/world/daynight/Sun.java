83779abed9e0f400e45170ae169131854cc8c747
yonicohen96 <61869789+yonicohen96@users.noreply.github.com>
2021-12-27 01:44:07 +0200

    sun rise

--- src/pepse/world/daynight/Sun.java
+++ src/pepse/world/daynight/Sun.java
@@ -2,13 +2,19 @@ package pepse.world.daynight;
 
 import danogl.GameObject;
 import danogl.collisions.GameObjectCollection;
+import danogl.components.Transition;
 import danogl.gui.rendering.OvalRenderable;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
 import java.awt.*;
+import java.beans.Transient;
 
 public class Sun extends GameObject{
+    // TODO check if should change according to window dims
+    private static final Vector2 SUN_SIZE = new Vector2(100, 100);
+
+
     /**
      * Construct a new GameObject instance.
      *
@@ -19,6 +25,7 @@ public class Sun extends GameObject{
      */
     public Sun(Vector2 topLeftCorner, Vector2 dimensions, Renderable renderable) {
         super(topLeftCorner, dimensions, renderable);
+
     }
 
     public static GameObject create(
@@ -28,10 +35,27 @@ public class Sun extends GameObject{
             float cycleLength){
         Renderable sunImg = new OvalRenderable(Color.YELLOW);
         Vector2 sunLocation = new Vector2(windowDimensions.x() / 2, windowDimensions.y() / 6);
-        Vector2 sunSize = new Vector2(windowDimensions.y() / 6, windowDimensions.y() / 6);
-        GameObject sun = new GameObject(sunLocation, sunSize, sunImg);
+        GameObject sun = new Sun(sunLocation, SUN_SIZE, sunImg);
         gameObjects.addGameObject(sun, layer);
+        new Transition<Float>(
+                sun, // the game object being changed
+                (angle) -> sun.setCenter(calcSunPosition(windowDimensions, angle)), // the method to call
+                0f, // initial transition value
+                (float) (2 * Math.PI), // final transition value
+                Transition.LINEAR_INTERPOLATOR_FLOAT, // use simple linear interpolator
+                cycleLength, // transition sun
+                Transition.TransitionType.TRANSITION_LOOP,
+                null); // nothing further to execute upon reaching final value
         return sun;
 
     }
+
+    private static Vector2 calcSunPosition(Vector2 windowDimensions, float angle) {
+        float radius = (windowDimensions.y() - SUN_SIZE.y()) / 2;
+        float y = (float) ((windowDimensions.y() / 2) - radius * Math.cos(angle));
+        float x = (float) ((windowDimensions.x() / 2) - radius * Math.sin(angle));
+        return new Vector2(x, y);
+    }
+
+
 }
