6c708bd8d3603738cb2bc1a33f60ea05dc1f4b85
yonicohen96 <61869789+yonicohen96@users.noreply.github.com>
2021-12-27 04:34:17 +0200

    leaves rotate

--- src/pepse/world/trees/Leaf.java
+++ src/pepse/world/trees/Leaf.java
@@ -1,5 +1,6 @@
 package pepse.world.trees;
 
+import danogl.components.Transition;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 import pepse.world.Block;
@@ -8,6 +9,15 @@ public class Leaf extends Block {
 
     public Leaf(Vector2 topLeftCorner, Renderable renderable) {
         super(topLeftCorner, renderable);
+        new Transition<Float>(
+                this, // the game object being changed
+                (angle) -> this.renderer().setRenderableAngle(angle), // the method to call
+                (float) (-5 * Math.PI), // initial transition value
+                (float) (5 * Math.PI), // final transition value
+                Transition.LINEAR_INTERPOLATOR_FLOAT, // use simple linear interpolator
+                1, // transition sun
+                Transition.TransitionType.TRANSITION_BACK_AND_FORTH,
+                null); // nothing further to execute upon reaching final value
     }
 
 }
