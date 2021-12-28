47276a2b1c0fbeed9866d0cac79311948785d118
yonicohen96 <61869789+yonicohen96@users.noreply.github.com>
2021-12-28 16:59:24 +0200

    leafs horizontal change

--- src/pepse/world/trees/Leaf.java
+++ src/pepse/world/trees/Leaf.java
@@ -21,9 +21,14 @@ public class Leaf extends Block {
         this.setVelocity(Vector2.ZERO);
         this.setTopLeftCorner(this.originalPosition);
         this.renderer().setOpaqueness(1);
-
-
     }
 
-
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        // todo check if to cha ge to states and to stop velocity if collided with ground
+        if (this.getVelocity().y() > 0){
+            this.transform().setTopLeftCornerX((float) (originalPosition.x() + Math.sin(this.getCenter().y() / 10) * 20));
+        }
+    }
 }
